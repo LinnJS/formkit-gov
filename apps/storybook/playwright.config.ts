cd5f288f0e40763765a9d1c1120f03f57c88e0cc
Justin Linn <justin@linnjs.com>
2025-12-01 00:32:52 -0500

    feat(storybook): Fix tests

--- apps/storybook/playwright.config.ts
+++ apps/storybook/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:6006',
+    baseURL: 'http://localhost:4002',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -34,7 +34,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm dev',
-    url: 'http://localhost:6006',
+    url: 'http://localhost:4002',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
